e1317a7c4d66a2bbd025721862835c83990e0e3c
Víctor Gauto <victor.gauto@outlook.com>
2024-12-23 20:15:06 -0300

    defino los íconos en soporte

--- scripts_quarto/soporte_quarto.R
+++ scripts_quarto/soporte_quarto.R
@@ -30,3 +30,12 @@ arcoiris <- glue(
   "<span style='color: {rainbow(n_col)}; font-size: 10px'>{cuadrado}</span>"
 ) |>
   str_flatten()
+
+# íconos
+i_descarga <- '<span class="nf nf-fa-download">'
+i_github <- '<span class="nf nf-fa-github">'
+i_email <- '<span class="nf nf-md-email"></span>'
+i_twitter <- '<span class="nf nf-cod-twitter">'
+i_instagram <- '<span class="nf nf-fa-instagram">'
+i_mastodon <- '<span class="nf nf-fa-mastodon">'
+i_bluesky <- '<span class="nf nf-fae-butterfly">'
